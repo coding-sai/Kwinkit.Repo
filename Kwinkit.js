10f3fc64d6437145f14030f5e828f1057cfe80f2
Jake_Silva <ceo@kwinkit.com>
2023-04-28 09:38:39 -0400

    initial commit

--- Kwinkit.js
+++ Kwinkit.js
@@ -1,1 +1,7 @@
-let greeting = "Hello, world!";
+modules.exports = {
+    port: Process.env.PORT,
+    files: ["./**/*.{html,htm,css,js}"],
+    server: {
+        baseDir: ["./src","./build/contracts"]
+    }
+}
