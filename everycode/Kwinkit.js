17f4d0a8131f391ea9307b673cc2cbda076e17b7
Jake_Silva <ceo@kwinkit.com>
2023-05-06 23:27:20 -0400

    some changes made in Kwinkit.js

--- everycode/Kwinkit.js
+++ everycode/Kwinkit.js
@@ -1,9 +1,9 @@
-modules.exports = {
-    port: Process.env.PORT,
-    files: ["./**/*.{html,htm,css,js}"],
-    server: {
-        baseDir: ["./src","./build/contracts"]
-    }
-}
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) =>
+    res.json{
+        heyguys: "helloworld",
+    });
 
 app.listen(process.env.PORT || 8080);
