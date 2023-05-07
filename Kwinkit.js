681cbb658624dff373e0cd86ce8839157f7797aa
Jake_Silva <ceo@kwinkit.com>
2023-05-06 22:24:01 -0400

    initial commit

--- Kwinkit.js
+++
@@ -1,9 +0,0 @@
-modules.exports = {
-    port: Process.env.PORT,
-    files: ["./**/*.{html,htm,css,js}"],
-    server: {
-        baseDir: ["./src","./build/contracts"]
-    }
-}
-
-let port = process.env.PORT || 8080
