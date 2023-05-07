2dd7b58b9779d099f8d1d605b4cd88288690c552
Jake_Silva <ceo@kwinkit.com>
2023-05-06 23:30:59 -0400

    added previous details

--- everycode/Kwinkit.js
+++ everycode/Kwinkit.js
@@ -1,3 +1,11 @@
+modules.exports = {
+    port: Process.env.PORT,
+    files: ["./**/*.{html,htm,css,js}"],
+    server: {
+        baseDir: ["./src","./build/contracts"]
+    }
+}
+
 const express = require("express");
 const app = express();
 
